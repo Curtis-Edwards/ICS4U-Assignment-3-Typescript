5671c667472305edfd7cc4c4a666c342da6c85e1
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-04-21 10:13:39 -0400

    Update Triangle.ts

--- Triangle.ts
+++ Triangle.ts
@@ -22,23 +22,87 @@ export class Triangle {
   }
 
   /*
-  * getter for sideA
+  * Getter for sideA
   */
   public get sideA() {
     return this.sideA
   }
 
   /*
-  * getter for sideB
+  * Getter for sideB
   */
   public get sideB() {
     return this.sideB
   }
 
   /*
-  * getter for sideC
+  * Getter for sideC
   */
   public get sideC() {
     return this.sideC
   }
+
+  /*
+  * Checks if the triangle has valid dimensions
+  */
+  public isValid(): boolean {
+  let isValid: boolean = true
+    if ((this.sideA + this.sideB) < this.sideC) {
+      isValid = false
+    } else if ((this.sideB + this.sideC) < this.sideA) {
+      isValid = false
+    } else if ((this.sideC + this.sideA) < this.sideB) {
+      isValid = false
+    }
+    return isValid
+  }
+
+  /*
+  * Calculates the semi-perimeter
+  */
+  public semiPerimeter(): number {
+    return (this.sideA + this.sideB + this.sideC) / 2
+  }
+
+  /*
+  * Calculates the area
+  */
+  /*public area(): number {
+
+  }
+
+  /*
+  * Finds the type of triangle
+  */
+  /*public getType(): string {
+
+  }
+
+  /*
+  * Calculates each angle of the triangle
+  */
+  /*public angle(angleNumber: number): number {
+
+  }
+
+  /*
+  * Calculates each height of the triangle
+  */
+  /*public height(sideNumber: number): number {
+
+  }
+
+  /*
+  * Calculates the inner circle radius
+  */
+  /*public innerCircleRadius(): number {
+
+  }
+
+  /*
+  * Calculates the circumsicle radius
+  */
+  /*public circumsicleRadius(): number {
+
+  }*/
 }
