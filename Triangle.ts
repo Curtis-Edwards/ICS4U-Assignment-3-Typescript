c8b03b17b364df61c1a352f76db1a9da3917fada
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-04-21 12:38:18 -0400

    Update Triangle.ts

--- Triangle.ts
+++ Triangle.ts
@@ -61,99 +61,127 @@ export default class Triangle {
   * Calculates the semi-perimeter
   */
   public semiPerimeter(): number {
-    return (this.sideA + this.sideB + this.sideC) / 2
+    if (isValid() === false) {
+      return -1
+    } else {
+      return (this.sideA + this.sideB + this.sideC) / 2
+    }
   }
 
   /*
   * Calculates the area
   */
   public area(): number {
-    return Math.sqrt(this.semiPerimeter()
-      * (this.semiPerimeter() - this.sideA)
-      * (this.semiPerimeter() - this.sideB)
-      * (this.semiPerimeter() - this.sideC)
-    )
+    if (isValid() === false) {
+      return -1
+    } else {
+      return Math.sqrt(this.semiPerimeter()
+        * (this.semiPerimeter() - this.sideA)
+        * (this.semiPerimeter() - this.sideB)
+        * (this.semiPerimeter() - this.sideC)
+      )
+    }
   }
 
   /*
   * Calculates each angle of the triangle
   */
   public angle(angleNumber: number): number {
-    let angle: number
-    if (angleNumber === 1) {
-      angle = Math.acos(
-        ((this.sideB ** 2) + (this.sideC ** 2) - (this.sideA ** 2))
-        / (2 * this.sideB * this.sideC)
-      )
-    } else if (angleNumber === 2) {
-      angle = Math.acos(
-        ((this.sideC ** 2) + (this.sideA ** 2) - (this.sideB ** 2))
-        / (2 * this.sideC * this.sideA)
-      )
-    } else if (angleNumber === 3) {
-      angle = Math.acos(
-        ((this.sideA ** 2) + (this.sideB ** 2) - (this.sideC ** 2))
-        / (2 * this.sideA * this.sideB)
-      )
+    if (isValid() === false) {
+      return -1
+    } else {
+      let angle: number
+      if (angleNumber === 1) {
+        angle = Math.acos(
+          ((this.sideB ** 2) + (this.sideC ** 2) - (this.sideA ** 2))
+          / (2 * this.sideB * this.sideC)
+        )
+      } else if (angleNumber === 2) {
+        angle = Math.acos(
+          ((this.sideC ** 2) + (this.sideA ** 2) - (this.sideB ** 2))
+          / (2 * this.sideC * this.sideA)
+        )
+      } else if (angleNumber === 3) {
+        angle = Math.acos(
+          ((this.sideA ** 2) + (this.sideB ** 2) - (this.sideC ** 2))
+          / (2 * this.sideA * this.sideB)
+        )
+      }
+      return angle
     }
-    return angle
   }
 
   /*
   * Finds the type of triangle
   */
   public getType(): string {
-    let triangleType: string
-    if (
-      this.sideA === this.sideB &&
-      this.sideB === this.sideC &&
-      this.sideC === this.sideA
-    ) {
-      triangleType = "Equilateral triangle"
-    } else if (
-      this.angle(1) === (Math.PI / 2) ||
-      this.angle(2) === (Math.PI / 2) ||
-      this.angle(3) === (Math.PI / 2)
-    ) {
-      triangleType = "Right angle triangle"
-    } else if (
-      this.sideA === this.sideB ||
-      this.sideB === this.sideC ||
-      this.sideC === this.sideA
-    ) {
-      triangleType = "Isosceles triangle"
-    } else if (this.sideA != this.sideB != this.sideC) {
-      triangleType = "Scalene triangle"
+    if (isValid() === false) {
+      return -1
+    } else {
+      let triangleType: string
+      if (
+        this.sideA === this.sideB &&
+        this.sideB === this.sideC &&
+        this.sideC === this.sideA
+      ) {
+        triangleType = "Equilateral triangle"
+      } else if (
+        this.angle(1) === (Math.PI / 2) ||
+        this.angle(2) === (Math.PI / 2) ||
+        this.angle(3) === (Math.PI / 2)
+      ) {
+        triangleType = "Right angle triangle"
+      } else if (
+        this.sideA === this.sideB ||
+        this.sideB === this.sideC ||
+        this.sideC === this.sideA
+      ) {
+        triangleType = "Isosceles triangle"
+      } else if (this.sideA != this.sideB != this.sideC) {
+        triangleType = "Scalene triangle"
+      }
+      return triangleType
     }
-    return triangleType
   }
 
   /*
   * Calculates each height of the triangle
   */
   public height(sideNumber: number): number {
-    let height: number
-    if (sideNumber === 1) {
-      height = 2 * this.area() / this.sideA
-    } else if (sideNumber === 2) {
-      height = 2 * this.area() / this.sideB
-    } else if (sideNumber === 3) {
-      height = 2 * this.area() / this.sideC
+    if (isValid() === false) {
+      return -1
+    } else {
+      let height: number
+      if (sideNumber === 1) {
+        height = 2 * this.area() / this.sideA
+      } else if (sideNumber === 2) {
+        height = 2 * this.area() / this.sideB
+      } else if (sideNumber === 3) {
+        height = 2 * this.area() / this.sideC
+      }
+      return height
     }
-    return height
   }
 
   /*
   * Calculates the inner circle radius
   */
   public innerCircleRadius(): number {
-    return this.area() / this.semiPerimeter()
+    if (isValid() === false) {
+      return -1
+    } else {
+      return this.area() / this.semiPerimeter()
+    }
   }
 
   /*
   * Calculates the circumsicle radius
   */
   public circumsicleRadius(): number {
-    return (this.sideA * this.sideB * this.sideC) / (4 * this.area())
+    if (isValid() === false) {
+      return -1
+    } else {
+      return (this.sideA * this.sideB * this.sideC) / (4 * this.area())
+    }
   }
 }
